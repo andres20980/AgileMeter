5f4fd384ea7284373c40e651872833d6506ac2e9
Jose Ant. Beltrán Márquez <bjabinn@hotmail.com>
2019-05-14 13:26:29 +0200

    testing to LIVE

--- everisapi.API/Startup.cs
+++ everisapi.API/Startup.cs
@@ -143,8 +143,7 @@ namespace everisapi.API
       app.Use(async (context, next) =>
       {
         await next();
-        if (context.Response.StatusCode == 404 &&
-           !Path.HasExtension(context.Request.Path.Value) &&
+        if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) &&
            !context.Request.Path.Value.StartsWith("/api/"))
         {
           context.Request.Path = "/index.html";
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -9,6 +9,6 @@
 
 export const environment = {
   production: false,
-  backendHost: "agilemeter", 
+  backendHost: "agilemeter.23.97.158.29.nip.io", 
   backendPort: 80,
 };
