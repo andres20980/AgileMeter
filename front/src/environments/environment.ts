576f4fdabe600f2dcf0b1c434f8c4874c10056fc
Jose Ant. Beltrán Márquez <bjabinn@hotmail.com>
2019-05-27 11:27:50 +0200

    Changing variable to PRE

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -9,6 +9,6 @@
 
 export const environment = {
   production: false,
-  backendHost: "agilemeter.23.97.158.29.nip.io", 
+  backendHost: "agilemeter-pre-back.23.97.158.29.nip.io", 
   backendPort: 80,
 };
