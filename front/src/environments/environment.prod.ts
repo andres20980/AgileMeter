794e78ecad6d523a3f8c494816adfcd2eb27bb39
@fvellari <fvellari@everis.com>
2019-06-03 21:38:17 +0100

    test 11

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   backendHost: "agilemeter-pre-back.23.97.158.29.nip.io", 
-  backendPort: 443,
+  backendPort: 443
 };
+//agilemeter-pre-back.23.97.158.29.nip.io
