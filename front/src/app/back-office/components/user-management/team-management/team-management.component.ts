68bc08852bf3879f911981a9055011b7a9f8ffd4
Francisco Javier Moreno Vicente <fmorenov@everis.com>
2019-06-03 07:14:45 +0200

    Añadimos los botonesgestión de usuario  y equipo

--- front/src/app/back-office/components/user-management/team-management/team-management.component.ts
+++ front/src/app/back-office/components/user-management/team-management/team-management.component.ts
@@ -2,12 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 
+//import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
+
 @Component({
   selector: 'app-team-management',
   templateUrl: './team-management.component.html',
   styleUrls: ['./team-management.component.scss']
 })
 export class TeamManagementComponent implements OnInit {
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
+
+  // drop(event: CdkDragDrop<string[]>) {
+  //   moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  // }
 
   constructor(
     private _router: Router,
