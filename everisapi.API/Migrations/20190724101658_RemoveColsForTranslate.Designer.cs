99ee296268c6c8a61992530bde23f35eb551b259
Adrian Giampaslia Dominguez <agiampas@everis.com>
2019-07-24 13:35:29 +0200

    Migration with delete cols

--- everisapi.API/Migrations/20190724101658_RemoveColsForTranslate.Designer.cs
+++ everisapi.API/Migrations/20190724101658_RemoveColsForTranslate.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace everisapi.API.Migrations
 {
     [DbContext(typeof(AsignacionInfoContext))]
-    partial class AsignacionInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20190724101658_RemoveColsForTranslate")]
+    partial class RemoveColsForTranslate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
