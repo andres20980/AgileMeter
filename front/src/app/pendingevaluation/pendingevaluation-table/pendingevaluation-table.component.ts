45da7f2684c9f82edf1ce8808d2d08299d46babb
Adrian Giampaslia Dominguez <agiampas@everis.com>
2019-08-02 12:35:28 +0200

    filtro eval pendientes sin el todas

--- front/src/app/pendingevaluation/pendingevaluation-table/pendingevaluation-table.component.ts
+++ front/src/app/pendingevaluation/pendingevaluation-table/pendingevaluation-table.component.ts
@@ -135,28 +135,25 @@ export class PendingEvaluationTableComponent implements OnInit {
   //Metodo encargado de refrescar la tabla 
   public refresh() {
     var o = this.OficinaSeleccionada;
-    //si es TODAS dejamos el array vacio  --> pasamos menos datos y excluimos una comparacion en la consulta --> mas optima
-    if (this.OficinaSeleccionada.length === 0 || this.OficinaSeleccionada[0] == "TODAS") {
-      o = [];//o = this.ListaDeOficinas;
-    }
+
     var e = [];
-    if (this.EquipoSeleccionado.length === 0 || this.EquipoSeleccionado[0].nombre == "TODAS") {
+    if (this.EquipoSeleccionado.length === 0) {
       e = [];
     } else {
       this.EquipoSeleccionado.forEach(function (element) {
         e.push(element.id);
       });
     }
-    var a = [] ;
-    if (this.AssessmentSeleccionado.length === 0 || this.AssessmentSeleccionado[0].assessmentName == "TODAS") {
+    var a = [];
+    if (this.AssessmentSeleccionado.length === 0) {
       a = [];
     } else {
       this.AssessmentSeleccionado.forEach(function (element) {
         a.push(element.assessmentId);
       });
     }
-    this.dataSource.filter ="";
-    this.parent.EvaluacionFiltrar ={ 'nombre': '', 'estado': 'false', 'fecha': '', 'userNombre': '', 'puntuacion': '', 'assessmentId': 0, 'oficinas':o, equipos:e, 'idAssessment': a};
+    this.dataSource.filter = "";
+    this.parent.EvaluacionFiltrar = { 'nombre': '', 'estado': 'false', 'fecha': '', 'userNombre': '', 'puntuacion': '', 'assessmentId': 0, 'oficinas': o, equipos: e, 'idAssessment': a };
     this.parent.GetPaginacion();
 
   }
@@ -258,9 +255,6 @@ export class PendingEvaluationTableComponent implements OnInit {
         this.ListaDeProyectos = res;
         this.getOficinasDeUsuario(res);
         this.ListaDeProyectosFiltrada = res;
-        //añadimos la opcion de todas al principio del select de equipos
-        this.ListaDeProyectosFiltrada.unshift({ id: -1, nombre: "TODAS", codigo: "", fecha: new Date(0, 0, 0), numFinishedEvals: 0, numPendingEvals: 0, oficina: "TODAS" });
-        this.EquipoSeleccionado = this.ListaDeProyectos;// inicialmente marcamos todas 
       },
       error => {
         //Si el servidor tiene algún tipo de problema mostraremos este error
@@ -285,68 +279,16 @@ export class PendingEvaluationTableComponent implements OnInit {
       }
     });
     this.ListaDeOficinas = oficinas.sort();
-    this.ListaDeOficinas.unshift("TODAS");
-    this.OficinaSeleccionada = this.ListaDeOficinas;// inicialmente marcamos todas las oficinas
-
   }
 
   public selectOficinas() {
-    //si no estan marcadas todas las casillas se desmarca la opcion de todas
-    if (this.OficinaSeleccionada[0] == "TODAS") {
-      if (this.OficinaSeleccionada.length < this.ListaDeOficinas.length) {
-        this.OficinaSeleccionada = this.OficinaSeleccionada.slice(1);
-      }
-    }
-    //si estan marcadas todas las casillas menos la opcion de todas --> se marca la opcion de todas
-    else {
-      if (this.OficinaSeleccionada.length == (this.ListaDeOficinas.length - 1)) {
-        this.OficinaSeleccionada = this.ListaDeOficinas;
-      }
-    }
-    //Modificamos el select del equipo y assessment mostrando los de las oficinas seleccionadas
     this.refresh();
     this.equiposDeLasOficinasSeleccionadas();
-  }
-  public seleccionarTodasOficinas() {
-    //marcamos / desmarcamos todas las opciones    
-    if (this.OficinaSeleccionada[0] === "TODAS") {
-      this.OficinaSeleccionada = this.ListaDeOficinas;
-    } else {
-      this.OficinaSeleccionada = [];
-    }
-    //Modificamos el select del equipo y assessment mostrando los de las oficinas seleccionadas
-    this.equiposDeLasOficinasSeleccionadas();
-    this.refresh();
   }
 
   // FUNCIONES SELECT DE EQUIPOS
   public selectEquipos() {
-    //comprobamos que al menos 1 este seleccionado    
-    if (this.EquipoSeleccionado.length > 0) {
-      //si no estan marcadas todas las casillas se desmarca la opcion de todas
-      if (this.EquipoSeleccionado[0].nombre == "TODAS") {
-        if (this.EquipoSeleccionado.length < this.ListaDeProyectosFiltrada.length) {
-          this.EquipoSeleccionado = this.EquipoSeleccionado.slice(1);
-        }
-      }
-      //si estan marcadas todas las casillas menos la opcion de todas --> se marca la opcion de todas
-      else {
-        if (this.EquipoSeleccionado.length == (this.ListaDeProyectosFiltrada.length - 1)) {
-          this.EquipoSeleccionado = this.ListaDeProyectos;
-        }
-      }
-    }
-    this.oficinasDeLosEquiposSeleccionados();
-    this.refresh();
-  }
-  public seleccionarTodosEquipos() {
-    //marcamos / desmarcamos todas las opciones    
-    if (this.EquipoSeleccionado[0].nombre === "TODAS") {
-      this.EquipoSeleccionado = this.ListaDeProyectos;
-    } else {
-      this.EquipoSeleccionado = [];
-    }
-    this.oficinasDeLosEquiposSeleccionados();
+    this.oficinasDeLosEquiposSeleccionados();    
     this.refresh();
   }
 
@@ -374,37 +316,10 @@ export class PendingEvaluationTableComponent implements OnInit {
       // a must be equal to b
       return 0;
     });
-    //añadimos la opcion de todas al principio del select de assessment
-    this.ListaDeAssessment.unshift({ assessmentId: -1, assessmentName: "TODAS" });
-    this.AssessmentSeleccionado = this.ListaDeAssessment;// inicialmente marcamos todas 
     this.ListaDeAssessmentFiltrada = this.ListaDeAssessment;
   }
 
   public selectAssessment() {
-    //comprobamos que al menos 1 este seleccionado
-    if (this.AssessmentSeleccionado.length > 0) {
-      //si no estan marcadas todas las casillas se desmarca la opcion de todas
-      if (this.AssessmentSeleccionado[0].assessmentName == "TODAS") {
-        if (this.AssessmentSeleccionado.length < this.ListaDeAssessment.length) {
-          this.AssessmentSeleccionado = this.AssessmentSeleccionado.slice(1);
-        }
-      }
-      //si estan marcadas todas las casillas menos la opcion de todas --> se marca la opcion de todas
-      else {
-        if (this.AssessmentSeleccionado.length == (this.ListaDeAssessment.length - 1)) {
-          this.AssessmentSeleccionado = this.ListaDeAssessment;
-        }
-      }
-    }
-    this.refresh();
-  }
-  public seleccionarTodosAssessment() {
-    //marcamos / desmarcamos todas las opciones    
-    if (this.AssessmentSeleccionado[0].assessmentName === "TODAS") {
-      this.AssessmentSeleccionado = this.ListaDeAssessment;
-    } else {
-      this.AssessmentSeleccionado = [];
-    }
     this.refresh();
   }
 
@@ -413,29 +328,15 @@ export class PendingEvaluationTableComponent implements OnInit {
     this.EquipoSeleccionado = [];
     this.ListaDeProyectosFiltrada = [];
 
-    if (this.OficinaSeleccionada.length === 0 || this.OficinaSeleccionada[0] === "TODAS") {
+    if (this.OficinaSeleccionada.length === 0) {
       this.ListaDeProyectosFiltrada = this.ListaDeProyectos;
     } else {
-      this.ListaDeProyectosFiltrada = this.ListaDeProyectos.filter(x => x.oficina === "TODAS")
-        .concat(this.ListaDeProyectos.filter(x => this.OficinaSeleccionada.indexOf(x.oficina) >= 0));
+      this.ListaDeProyectosFiltrada = this.ListaDeProyectos.filter(x => this.OficinaSeleccionada.indexOf(x.oficina) >= 0);
     }
-    this.EquipoSeleccionado = this.ListaDeProyectosFiltrada;
   }
 
   public oficinasDeLosEquiposSeleccionados() {
-    if (this.EquipoSeleccionado.length === 0 || this.EquipoSeleccionado[0].oficina === "TODAS") {
-      if (this.EquipoSeleccionado.length !== 0 && this.EquipoSeleccionado[0].oficina === "TODAS") {
-        var o = [];
-        this.ListaDeProyectosFiltrada.forEach(function (element) {
-          if (element.oficina != "TODAS") {
-            o.push(element.oficina);
-          }
-        });
-        this.OficinaSeleccionada = o;
-      } else {
-        this.OficinaSeleccionada = [];
-      }
-    } else {
+    if (this.EquipoSeleccionado.length != 0) {
       var oficinas = [];
       this.EquipoSeleccionado.forEach(function (value) {
         if (oficinas.indexOf(value.oficina) < 0) {
