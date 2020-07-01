37671db0fd6b807147c6c24e57e06b5e7da004a1
Cristian Antonio Gonzalez Robles <cgonzaro@everis.com>
2020-07-01 09:31:48 +0200

    Crear una tarjeta contenedora del mensaje de correo

--- everisapi.API/Controllers/Helper.cs
+++ everisapi.API/Controllers/Helper.cs
@@ -9,7 +9,7 @@ using System.IO;
 public static class Helper
 {
 
-    public static void emailBuilder(MimeMessage message, String user, String pass)
+    public static void emailBuilder(MimeMessage message, String user, String pass, String nombreCompleto)
     {
 
         var builder = new BodyBuilder();
@@ -18,30 +18,32 @@ public static class Helper
 
         // Set the html version of the message text
         builder.HtmlBody = string.Format(@"
-            <div style='padding:20px; font-family: Calibri,Candara,Segoe,Segoe UI,Optima,Arial,sans-serif;font-size:18px;'>
-            <center><a href='https://agilemeter.everis.com/'><img width='65%' height='50%' alt='AgileMeter' src=""cid:{0}""></a></center>
-            <br>
-            <hr>
-            <br>
-            <center><strong>¡Bienvenido/a a AgileMeter!</strong></center>
-            <br>
-            <p>Estamos encantados de que formes parte de nuestra familia y que te podamos ayudar a evaluar el nivel de madurez en Agile de tus equipos.
-            Para comenzar, te facilitamos las credenciales que tendrás que usar para acceder a nuestra plataforma:
-            </p>
-            <ul>
-                <li>URL: <a href='https://agilemeter.everis.com/'>https://agilemeter.everis.com/</a></li>
-                <li>Usuario: {1}</li>
-                <li>Contraseña: {2}</li>
-            </ul> 
-            <br>
-            <hr>
-            <br>
-            <strong>¿Tienes dudas? ¿Te podemos ayudar?</strong>
-            
-            <p>Escribenos a agile.meter@everis.com, estaremos encantados de ayudarte.</p><br>
-            <center>¡Qué tengas un día maravilloso!</center>
+            <div style='padding-bottom:20px;font-family: Calibri,Candara,Segoe,Segoe UI,Optima,Arial,sans-serif;font-size:18px;width: 620px;margin: 0 auto;'>
+            <center><a style='margin-right:10px;margin-top:2px' href='https://agilemeter.everis.com/'><img alt='AgileMeter' src=""cid:{0}""></a></center>
+                <br>
+    <center><strong>¡Bienvenido/a {3} a AgileMeter!</strong></center>
+    <br>
+    <p style='padding-left:20px;padding-right:20px'>Estamos encantados de que formes parte de nuestra familia y que te
+        podamos ayudar a evaluar el nivel de madurez en Agile de tus equipos.
+        Para comenzar, te facilitamos las credenciales que tendrás que usar para acceder a nuestra plataforma:
+    </p>
+    <div style='padding-left:20px;padding-right:20px'>
+        <ul>
+            <li>URL: <a href='https://agilemeter.everis.com/'>https://agilemeter.everis.com/</a></li>
+            <li>Usuario: {1}</li>
+            <li>Contraseña: {2}</li>
+        </ul>
+    </div>
+    <br>
+    <hr>
+    <br>
+    <strong style='padding-left:20px;padding-right:20px'>¿Tienes dudas? ¿Te podemos ayudar?</strong>
 
-            </div>", image.ContentId, user, pass);
+    <p style='padding-left:20px;padding-right:20px'>Escribenos a agile.meter@everis.com, estaremos encantados de
+        ayudarte.</p><br>
+    <center>¡Qué tengas un día maravilloso!</center>
+
+</div> ", image.ContentId, user, pass, nombreCompleto);
 
 
 
